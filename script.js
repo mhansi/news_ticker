c5baa1dfa21887403ef01c8b5dcaf0e4ca5792be
mhansi <srsmsone@gmail.com>
2019-05-06 23:48:57 +0530

    Create Repo

---
+++ script.js
@@ -0,0 +1,17 @@
+var interval;
+function startTicker(){
+    $("#news li:first").slideUp(function(){
+        $(this).appendTo($("#news")).slideDown();
+    });
+}
+function stopTicker(){
+    clearInterval(interval);
+}
+$(document).ready(function(){
+    interval=setInterval(startTicker,2000);
+    $("#news").hover(function(){
+        stopTicker();
+    },function(){
+        interval=setInterval(startTicker,2000);
+    });
+});
